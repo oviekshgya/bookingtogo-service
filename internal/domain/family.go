4f83b28acde1c03a26325c618fd7965960bb40dd
oviekshgya <oviekshgy@gmail.com>
2025-08-15 13:36:29 +0700

    ADd

--- internal/domain/family.go
+++ internal/domain/family.go
@@ -6,8 +6,8 @@ type Family struct {
 	ID         int    `gorm:"primaryKey;autoIncrement;column:fl_id"`
 	CustomerID int    `gorm:"not null;column:cat_id"`
 	Relation   string `gorm:"size:50;not null;column:fl_relation"`
-	Name       string `gorm:"size:50;not null;column:fl_name"`
-	DOB        string `gorm:"size:50;not null;column:fl_dob"` // bisa string atau time.Time
+	Name       string `gorm:"size:50;not null;column:fl_name" json:"name"`
+	DOB        string `gorm:"size:50;not null;column:fl_dob" json:"DOB"` // bisa string atau time.Time
 }
 
 func (Family) TableName() string {
